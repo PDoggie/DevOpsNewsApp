19e7d687f027cabff503b2920ef5a7afb3c40cfb
PDoggie <148388896+PDoggie@users.noreply.github.com>
2023-10-19 09:05:08 -0500

    Initial commit

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json.Linq;
+
+public class IndexModel : PageModel
+{
+    private readonly IHttpClientFactory _clientFactory;
+    public IEnumerable<string> News { get; private set; }
+
+    public IndexModel(IHttpClientFactory clientFactory)
+    {
+        _clientFactory = clientFactory;
+    }
+
+    public async Task OnGetAsync()
+    {
+        var client = _clientFactory.CreateClient();
+        var response = await client.GetStringAsync("https://newsapi.org/v2/everything?q=DevOps&apiKey=413b5126c97b407395eb0bac6bb8fc81");
+
+        var json = JObject.Parse(response);
+        News = json["articles"].ToObject<List<JToken>>().ConvertAll(article => article["title"].ToString());
+    }
+}
